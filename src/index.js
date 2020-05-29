fa07431eceba0548f2b9aac3b173ab07ffee25ed
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-05-29 18:33:12 +0100

    created screens folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Home from './screen/Home';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
