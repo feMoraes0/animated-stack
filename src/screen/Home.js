0291cae628b8d73716ec125c2cf89a790c5e1ec6
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-05-29 19:22:00 +0100

    cards added

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './Home.css';
 
 function Home() {
   return (
-    <div>
-      <p>Hello World</p>
+    <div className='home-body'>
+      <div className='page-card card1'>
+        <p>1</p>
+      </div>
+      <div className='page-card card2'>
+        <p>2</p>
+      </div>
+      <div className='page-card card3'>
+        <p>3</p>
+      </div>
+      <div className='page-card card4'>
+        <p>4</p>
+      </div>
     </div>
   );
 }
