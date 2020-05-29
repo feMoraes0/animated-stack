b22962bc99862c2bd1ee5cf67797f619fd893395
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-05-29 21:23:25 +0100

    animation concept initialized

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -2,8 +2,31 @@ import React from 'react';
 import './Home.css';
 
 function Home() {
+  function handleAnimations() {
+    const card = document.getElementsByClassName('card1')[0];
+    const card2 = document.getElementsByClassName('card2')[0];
+    const card3 = document.getElementsByClassName('card3')[0];
+    const card4 = document.getElementsByClassName('card4')[0];
+    if (card != null) {
+      card.classList.add('card-depois');
+      setInterval(() => {
+        card.classList.remove('card-depois');
+      }, 1500);
+      setInterval(() => {
+        card.classList.replace('card1', 'card4');
+        card2.classList.replace('card2', 'card1');
+        card3.classList.replace('card3', 'card2');
+        card4.classList.replace('card4', 'card3');
+      }, 1000);
+    }
+
+
+    console.log(card);
+  }
+
   return (
     <div className='home-body'>
+      <button onClick={() => handleAnimations()} type='button'>clique</button>
       <div className='page-card card1'>
         <p>1</p>
       </div>
