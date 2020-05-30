2a0121422f0f2ab76ea296185c579d0310523d4d
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-05-30 13:37:41 +0100

    animation cards created

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -2,42 +2,73 @@ import React from 'react';
 import './Home.css';
 
 function Home() {
-  function handleAnimations() {
-    const card = document.getElementsByClassName('card1')[0];
-    const card2 = document.getElementsByClassName('card2')[0];
-    const card3 = document.getElementsByClassName('card3')[0];
-    const card4 = document.getElementsByClassName('card4')[0];
-    if (card != null) {
-      card.classList.add('card-depois');
-      setInterval(() => {
-        card.classList.remove('card-depois');
-      }, 1500);
-      setInterval(() => {
-        card.classList.replace('card1', 'card4');
-        card2.classList.replace('card2', 'card1');
-        card3.classList.replace('card3', 'card2');
-        card4.classList.replace('card4', 'card3');
-      }, 1000);
-    }
+  const children = ['card-1', 'card-2', 'card-3', 'card-4'];
 
+  /**
+   * @param {List} elements
+   * @param {String} currentElementId
+   */
+  function handleClassesSwapAnimation(elements, currentElementId) {
+    const currentElementValue = document.getElementById(currentElementId).classList.value;
+    let nextElementValue = currentElementValue.replace(' go-next', '');
 
-    console.log(card);
+    elements.forEach((element) => {
+      const elementValue = document.getElementById(element).classList.value;
+      document.getElementById(element).classList.value = nextElementValue;
+      nextElementValue = elementValue;
+    });
+
+    const removeInterval = setInterval(() => {
+      document.getElementById(currentElementId).classList.value = nextElementValue;
+      document.getElementById(currentElementId).classList.remove('go-next');
+      clearInterval(removeInterval);
+    }, 250);
+  }
+
+  function nextCard() {
+    const actualCard = children.shift();
+    const element = document.getElementById(actualCard).classList;
+
+    element.add('go-next');
+
+    const swapInterval = setInterval(() => {
+      handleClassesSwapAnimation(children, actualCard);
+      children.push(actualCard);
+      clearInterval(swapInterval);
+    }, 500);
+  }
+
+  function previousCard() {
+    const lastCard = children.pop();
+    const element = document.getElementById(lastCard).classList;
+
+    element.add('go-next');
+
+    const swapInterval = setInterval(() => {
+      handleClassesSwapAnimation(children.reverse(), lastCard);
+      children.push(lastCard);
+      children.reverse();
+      clearInterval(swapInterval);
+    }, 500);
   }
 
   return (
     <div className='home-body'>
-      <button onClick={() => handleAnimations()} type='button'>clique</button>
-      <div className='page-card card1'>
-        <p>1</p>
-      </div>
-      <div className='page-card card2'>
-        <p>2</p>
-      </div>
-      <div className='page-card card3'>
-        <p>3</p>
-      </div>
-      <div className='page-card card4'>
-        <p>4</p>
+      <button onClick={() => nextCard()} type='button'>Go Next</button>
+      <button onClick={() => previousCard()} type='button'>Go Back</button>
+      <div id='cards'>
+        <div id='card-1' className='page-card card1'>
+          <p>1</p>
+        </div>
+        <div id='card-2' className='page-card card2'>
+          <p>2</p>
+        </div>
+        <div id='card-3' className='page-card card3'>
+          <p>3</p>
+        </div>
+        <div id='card-4' className='page-card card4'>
+          <p>4</p>
+        </div>
       </div>
     </div>
   );
